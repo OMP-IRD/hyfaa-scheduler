5fb19373a9ae82c3333cc604ff4c2f166681eae6
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2021-04-09 09:31:11 +0200

    Add shell instructions. Remove ensemblist run

--- work_configurations/operational_niger_gsmap/run.sh
+++ work_configurations/operational_niger_gsmap/run.sh
@@ -1,4 +1,4 @@
-set -e 
+set -e
 
 #hyfaa_workdir, hyfaa_temp_dir and hyfaa_config_folder variables must be set not only for this script but also because they are used in configuration files (in hyfaa_config_folder)
 
@@ -16,26 +16,31 @@ fi
 
 ###########
 #get forcing and assimilated data : common step for assimilated, ensemblist and mgbstandard solutions
+echo "get forcing and assimilated data : common step for assimilated, ensemblist and mgbstandard solutions"
 hyfaa_preprocessing_forcing.py --input_yaml_file ${hyfaa_workdir}/config/input_assimilated_solution.yaml
 hyfaa_preprocessing_assimilation.py --input_yaml_file ${hyfaa_workdir}/config/input_assimilated_solution.yaml
 
 
 ###########
 #process mgbstandard solution
+echo "# process mgbstandard solution"
 hyfaa_processing.py --input_yaml_file ${hyfaa_workdir}/config/input_mgbstandard_solution.yaml
 hyfaa_postprocessing.py --input_yaml_file ${hyfaa_workdir}/config/input_mgbstandard_solution.yaml
 
 #process ensemblist solution
-hyfaa_processing.py --input_yaml_file ${hyfaa_workdir}/config/input_ensemblist_solution.yaml
-hyfaa_postprocessing.py --input_yaml_file ${hyfaa_workdir}/config/input_ensemblist_solution.yaml
+# echo "# process ensemblist solution"
+# hyfaa_processing.py --input_yaml_file ${hyfaa_workdir}/config/input_ensemblist_solution.yaml
+# hyfaa_postprocessing.py --input_yaml_file ${hyfaa_workdir}/config/input_ensemblist_solution.yaml
 
 #process assimilated solution
+echo "# process assimilated solution"
 hyfaa_processing.py --input_yaml_file ${hyfaa_workdir}/config/input_assimilated_solution.yaml
 hyfaa_postprocessing.py --input_yaml_file ${hyfaa_workdir}/config/input_assimilated_solution.yaml
 
 
 ###########
 #create fake previsions using previous years
+echo "# create fake previsions using previous years"
 fake_previsions_using_previous_years_forcing.py --input_yaml_file ${hyfaa_workdir}/config/input_mgbstandard_solution.yaml --ndays 10 --nyearsmax 10
 fake_previsions_using_previous_years_forcing.py --input_yaml_file ${hyfaa_workdir}/config/input_ensemblist_solution.yaml --ndays 10 --nyearsmax 10
 fake_previsions_using_previous_years_forcing.py --input_yaml_file ${hyfaa_workdir}/config/input_assimilated_solution.yaml --ndays 10 --nyearsmax 10
